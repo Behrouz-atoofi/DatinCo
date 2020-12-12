2661a9d1d9e5fbb68a16fdb5c1714d640fdc311f
Behrouz-atoofi <40962954+Behrouz-atoofi@users.noreply.github.com>
2020-12-13 01:22:39 +0330

    update

--- elmsV1/src/main/java/main.java
+++ elmsV1/src/main/java/main.java
@@ -9,24 +9,6 @@ public class main {
 
     public static void main(String[] args) {
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("elms");
-        EntityManager entityManager = factory.createEntityManager();
 
-        EmployeeDTO employeeDTO = new EmployeeDTO();
-        employeeDTO.setName("javad");
-        employeeDTO.setFamily("yasari");
-        employeeDTO.setUsername("123");
-        employeeDTO.setPassword("123");
-        employeeDTO.setEmail("hasani@gmail.com");
-        employeeDTO.setManager(employeeDTO);
-
-        Category_elementDTO role = new Category_elementDTO();
-        role.setElement_id(3);
-        employeeDTO.setRole(role);
-        System.out.println(role.getCategory());
-
-        entityManager.getTransaction().begin();
-        entityManager.persist(employeeDTO);
-        entityManager.getTransaction().commit();
     }
 }
